ffd19f52918dc73a4af4cabf036d0b287ce1bf2d
NoaJohansson <noajoh517@edu.linkoping.se>
2025-05-15 12:08:51 +0000

    tre färger + js fix

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 const buttonconst = document.getElementById("color-button");
+let numb = 0;
 
-if(buttonconst) {
+if (buttonconst) {
     buttonconst.addEventListener("click", function () {
-
-    console.log("det fungerar");
-    document.documentElement.style.setProperty('--bodycolor', 'lightpink');
-    localStorage.setItem('bakgrundsfarg', 'lightpink');
-
-
-})
+        if (numb === 0) {
+            console.log("pink");
+            document.documentElement.style.setProperty('--bodycolor', 'lightpink');
+            localStorage.setItem('bakgrundsfarg', 'lightpink');
+            numb = 1;
+        } else if (numb === 1) {
+            console.log("blue");
+            document.documentElement.style.setProperty('--bodycolor', 'lightblue');
+            localStorage.setItem('bakgrundsfarg', 'lightblue');
+            numb = 2;
+        }
+        else {
+            console.log("gray");
+            document.documentElement.style.setProperty('--bodycolor', 'lightgray');
+            localStorage.setItem('bakgrundsfarg', 'lightgray');
+            numb = 0;
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     const savedColor = localStorage.getItem('bakgrundsfarg');
-    console.log(savedColor);
     if (savedColor) {
         document.documentElement.style.setProperty('--bodycolor', savedColor);
+        if (savedColor === 'lightpink') {
+            numb = 1;
+        } else if(savedColor === 'lightblue') {
+            numb = 2;
+        }
+        else{
+             numb = 0;
+        }
     }
 });
